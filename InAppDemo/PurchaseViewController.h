420ebfd6231adffdb9ae7b20679062c0beb3f3e4
Lailei Huang (Alex) <laileihuang@gmail.com>
2014-07-28 16:01:15 +1000

    add the PurchaseViewController

---
+++ InAppDemo/PurchaseViewController.h
@@ -0,0 +1,25 @@
+//
+//  PurchaseViewController.h
+//  InAppDemo
+//
+//  Created by Lailei Huang on 28/07/2014.
+//  Copyright (c) 2014 alex.huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <StoreKit/StoreKit.h>
+
+@interface PurchaseViewController : UIViewController <SKPaymentTransactionObserver, SKProductsRequestDelegate>
+
+@property (strong, nonatomic) IBOutlet UILabel *productTitle;
+
+@property (strong, nonatomic) IBOutlet UITextView *productDescription;
+
+@property (strong, nonatomic) IBOutlet UIButton *buyButton;
+
+@property (strong, nonatomic) SKProduct *product;
+@property (strong, nonatomic) NSString *productID;
+
+-(void)getProductInfo:(UIViewController *)viewController;
+
+@end
---
+++ InAppDemo/PurchaseViewController.m
@@ -0,0 +1,38 @@
+//
+//  PurchaseViewController.m
+//  InAppDemo
+//
+//  Created by Lailei Huang on 28/07/2014.
+//  Copyright (c) 2014 alex.huang. All rights reserved.
+//
+
+#import "PurchaseViewController.h"
+
+@interface PurchaseViewController ()
+
+@end
+
+@implementation PurchaseViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
